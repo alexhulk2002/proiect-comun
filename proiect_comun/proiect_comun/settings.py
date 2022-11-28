231b06d1a9cca234291c51d35f495d7973aaf5c8
alexhulk2002 <75840346+alexhulk2002@users.noreply.github.com>
2022-11-28 23:19:01 +0200

    .

--- proiect_comun/proiect_comun/settings.py
+++ proiect_comun/proiect_comun/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_ROOT = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'cazare/media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
