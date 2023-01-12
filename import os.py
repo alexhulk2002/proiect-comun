771ca8413f4c49282c4d0c906b0cf8a669e38db7
alexhulk2002 <75840346+alexhulk2002@users.noreply.github.com>
2023-01-12 22:10:03 +0200

    deschidere automata proiect

---
+++ import os.py
@@ -0,0 +1,22 @@
+import os
+
+
+try:
+    os.system('cmd /k "app\\Scripts\\activate.bat"')
+except:
+    print('error')
+
+try:
+    os.system('cmd /k "pip install django"')
+except:
+    print('error')
+
+try:
+    os.system('cmd /k "cd proiect_comun"')
+except:
+    print('error')
+
+try:
+    os.system('cmd /k "py manage.py runserver"')
+except:
+    print('error')
