4c2a148a307c1cdffba697dbe5c46f243803d626
alexhulk2002 <75840346+alexhulk2002@users.noreply.github.com>
2023-02-07 23:26:28 +0200

    new modificat

--- proiect_comun/app1/forms.py
+++ proiect_comun/app1/forms.py
@@ -6,3 +6,16 @@ class newform(ModelForm):
     class Meta:
         model= Cazare
         fields = "__all__"
+        labels={
+            'title': 'Titlu',
+            'description': 'Descriere',
+            'author': 'Autor',
+            'adresa': 'Adresa',
+            'photos': 'Poza',
+        }
+        widgets ={
+            'title': forms.TextInput(attrs={'class':'new', 'required':''}),
+            'description': forms.TextInput(attrs={'class':'new', 'required':''}),
+            'author':forms.TextInput(attrs={'class':'new', 'required':''}),
+            'adresa':forms.TextInput(attrs={'class':'new', 'required':''}),
+        }
